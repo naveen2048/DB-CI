4a303b42640c7123f95010b83de83674fedf1402
Sambav1 <Sambav1@LAPTOP-SAMBAV1>
2017-04-13 03:26:05 +0530

    Added new SQL DB project

---
+++ DB-CI/DB-CI/dbo/Tables/Country.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Country] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Country]  NVARCHAR (100) NULL,
+    [Currency] CHAR (3)       NULL,
+    CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
