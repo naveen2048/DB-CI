30e52206c86192cbe0a59c41c35205359e94aec3
Sambav1 <Sambav1@LAPTOP-SAMBAV1>
2017-04-13 23:26:25 +0530

    removed columns

--- DB-CI/DB-CI/dbo/Tables/Country.sql
+++ DB-CI/DB-CI/dbo/Tables/Country.sql
@@ -2,9 +2,6 @@
     [Id]       INT            IDENTITY (1, 1) NOT NULL,
     [Country]  NVARCHAR (100) NULL,
     [Currency] CHAR (3)       NULL,
-    [Zip] NCHAR(15) NULL, 
-    [Test] NCHAR(10) NULL, 
-    [Col1] NCHAR(10) NULL, 
     CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
