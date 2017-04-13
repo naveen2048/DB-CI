2ad4853f8c40013e9c3c360649ab8bdbd5663d92
Sambav1 <Sambav1@LAPTOP-SAMBAV1>
2017-04-13 21:36:48 +0530

    reduce Zip column to 9 chars

--- DB-CI/DB-CI/dbo/Tables/Country.sql
+++ DB-CI/DB-CI/dbo/Tables/Country.sql
@@ -2,7 +2,7 @@
     [Id]       INT            IDENTITY (1, 1) NOT NULL,
     [Country]  NVARCHAR (100) NULL,
     [Currency] CHAR (3)       NULL,
-    [Zip] NCHAR(15) NULL, 
+    [Zip] NCHAR(9) NULL, 
     CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
